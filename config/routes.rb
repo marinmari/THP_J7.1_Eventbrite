967a6559e5775e7777aa36eb18957ccf37cf851f
Marin <marin.chretien@gmail.com>
2021-08-10 11:18:40 +0200

    on veut lancer Heroku please

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+  get 'static_pages/secret'
+  devise_for :users
   get 'users/new'
   get 'users/create'
   get 'users/index'
   get 'users/destroy'
   get 'users/update'
   get 'users/show'
+  root 'static_pages#index'
+  get 'static_pages/secret' 
 resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
